47ba09c27097e2b0b25fbe04ad310ba5bdc8c7a7
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2026-03-06 15:22:11 -0300

    chore(39): add command check-types and fix problems

--- apps/details-game-screen/vite.config.ts
+++ apps/details-game-screen/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
+  plugins: [devtools(), solidPlugin() as PluginOption],
   base: '/details-game',
   server: {
     port: 3004,
