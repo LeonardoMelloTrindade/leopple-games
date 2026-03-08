aaa25b79f0e6273b9f1605470f22b2d33116ad87
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2026-03-08 02:03:31 -0300

    chore(41): create dockerfile

---
+++ .changeset/gold-toys-rescue.md
@@ -0,0 +1,5 @@
+---
+"games-list-screen": patch
+---
+
+chore(41): add create dockerfile for app game-list-screen
--- apps/games-list-screen/vite.config.ts
+++ apps/games-list-screen/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/games',
   server: {
-    port: 3001
+    port: 3001,
+    host: true
   },
   plugins: [react()],
 })
