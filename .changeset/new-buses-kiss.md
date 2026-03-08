1b8360e303d27e671aa459e5139ad5bd8716144b
Leonardo Mello Trindade <91926132+LeonardoMelloTrindade@users.noreply.github.com>
2026-03-08 01:42:41 -0300

    chore(41): create dockerfile login-screen (#14)

---
+++ .changeset/new-buses-kiss.md
@@ -0,0 +1,5 @@
+---
+"login-screen": patch
+---
+
+chore(41): create Dockerfile from app login-screen
--- apps/login-screen/vite.config.ts
+++ apps/login-screen/vite.config.ts
@@ -7,7 +7,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   base: '/login',
   server: {
-    port: 3000
+    port: 3000,
+    host: true
   },
   plugins: [
     vue(),
