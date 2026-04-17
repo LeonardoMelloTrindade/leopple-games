4f7c543845fb4e7135991cddcace4b95bf9af713
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2026-04-17 18:33:23 -0300

    style(15): add lib bootstrap for vue

---
+++ .changeset/busy-carpets-rest.md
@@ -0,0 +1,5 @@
+---
+"@leopple-games/styles": patch
+---
+
+style(15): add lib bootstrap-vue-next for vue in app login-screen
--- apps/login-screen/src/main.ts
+++ apps/login-screen/src/main.ts
@@ -2,10 +2,13 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
+
 import '@leopple-games/styles/normalize';
+import { createBootstrap } from '@leopple-games/styles/bootstrap/vue-bootstrap';
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(createBootstrap());
 
 app.mount('#app');
