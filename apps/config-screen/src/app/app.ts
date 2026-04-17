b7d421097fbf3d89cc2dbd77812ebf03123636b8
Leonardo Mello Trindade <91926132+LeonardoMelloTrindade@users.noreply.github.com>
2026-04-17 19:59:04 -0300

    style(16): add bootstrap for angular (#42)

--- apps/config-screen/src/app/app.ts
+++ apps/config-screen/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import '@leopple-games/styles/normalize';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbAlertModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
