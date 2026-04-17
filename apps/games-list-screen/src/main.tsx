f678ce3cba8eb30aa45f5c5fd4fbc7e22ddbc8ee
Leonardo Mello Trindade <91926132+LeonardoMelloTrindade@users.noreply.github.com>
2026-04-17 19:33:46 -0300

    style(14): add react-bootstrap for games-list-screen app (#41)

--- apps/games-list-screen/src/main.tsx
+++ apps/games-list-screen/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import '@leopple-games/styles/normalize';
+import '@leopple-games/styles/bootstrap/react-bootstrap';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
