60d990eea1c466cfbbe5bb87c9de35969ff7f74b
Leonardo Mello Trindade <91926132+LeonardoMelloTrindade@users.noreply.github.com>
2026-04-20 22:45:16 -0300

    feat(36): create module controller service s3 (#53)

--- apps/backend/src/config/envs/env.config.ts
+++ apps/backend/src/config/envs/env.config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { IPostgreSqlEnv, IRedisEnv } from './env.interface';
+import { IPostgreSqlEnv, IRedisEnv, IS3Env } from './env.interface';
 
 const postgreSqlEnv: IPostgreSqlEnv = {
   host: process.env.POSTGRESQL_HOST || '',
@@ -15,4 +15,8 @@ const redisEnv: IRedisEnv = {
   password: process.env.REDIS_PASSWORD || '',
 };
 
-export { postgreSqlEnv, redisEnv };
+const s3Envs: IS3Env = {
+  s3_url: process.env.S3_URL || 'http://localhost:9444/',
+};
+
+export { postgreSqlEnv, redisEnv, s3Envs };
